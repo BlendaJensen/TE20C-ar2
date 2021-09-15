8e966e7dcf5ccd1650c457384fd1729ef1662ba5
BlendaJensen <fredrikssonjensen.blenda@elev.ga.ntig.se>
2021-09-15 11:07:09 +0200

    Lucktext övning

---
+++ Kapitel 2/Lucktext/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Detta är ett program där du skriver in namn och ålder på en karaktär. Du skriver även in en plats.");
+
+            Console.Write("Skriv in karaktärens namn: ");
+            string namn = Console.ReadLine();
+
+            Console.Write("Skriv in karaktärens ålder: ");
+            string ålder = Console.ReadLine();
+
+            Console.Write("Skriv in en plats: ");
+            string plats = Console.ReadLine();
+
+            Console.WriteLine($"{namn} var {ålder} år. {namn} var ute för att gå till {plats} och på vägen hände det något. ");
+        }
+    }
+}
---
+++ Kapitel 2/Övning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Detta program frågar vad du heter och bor");
+
+            Console.Write("Vad heter du? ");
+
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Hej på dig" + namn);
+
+            Console.Write("Vart bor du? ");
+            string ort = Console.ReadLine();
+            Console.WriteLine("Vad trevligt att bo i " + ort);
+
+            Console.WriteLine("Hur gammal är du? ");
+            string ålder = Console.ReadLine();
+            Console.WriteLine("Du är ålder gammal");
+        }
+    }
+}
