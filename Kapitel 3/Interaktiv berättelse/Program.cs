d8e51157ee41abe0990968307c31b0964e11f369
BlendaJensen <fredrikssonjensen.blenda@elev.ga.ntig.se>
2021-09-22 11:08:15 +0200

    Tränat på if-satser och tolower

---
+++ Kapitel 3/Interaktiv berättelse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interaktiv_berättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Berättar vad det handlar om
+            Console.Write("Välkomen till spelet, du försöker komma undan en mördare och för att leva måste du göra rätt val. Om du vill stanna där du är skriv stanna. Om du vill gå fram skriv fram: ");
+            string rörelseEtt = Console.ReadLine().ToLower();
+            
+            //Vad som händer om man stannar
+            if ( rörelseEtt == "stanna")
+            {
+                Console.WriteLine("Du dog och det blir ett olöst mordfall");
+                Console.WriteLine("Du förlorade!");
+            }
+            //Vad som händer om man fortsätter fram
+            if (rörelseEtt == "fram")
+            {
+                Console.WriteLine("Du kommer fram till ett hus med två dörrar, tar du vänster eller höger dörr?");
+                string rörelseTvå = Console.ReadLine().ToLower();
+                
+                if (rörelseTvå == "vänster")
+                {
+                    Console.WriteLine("Du kommer in till ett tomt rum och det finns ett hål i mitten. Går du fram till hålet?");
+                    string rörelseTreV = Console.ReadLine().ToLower();
+                    
+                    if (rörelseTreV == "ja")
+                    {
+                        Console.WriteLine("Du går sakta fram till hålet. När du sätter dig ner och lutar över hålet så känns det som att en hand trycks mot din rygg och du faller ner i hålet.");
+                        Console.Write("Du förlorade!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du tycker att hålet är mistänksamt och väljer att inte att gå fram till hålet. Men du märker inte att någon kommer in i rummet och världen svartnar för dina ögon.");
+                        Console.WriteLine("Du förlorade!");
+                    }
+                    if (rörelseTvå == "höger")
+                    {
+                        Console.WriteLine("Du kom in i ett rum som har en vägg full med tavlor, en stor dörr och en spegel. Vad går du till?");
+                        string rörelseTreH = 
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel 3/Villkor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Villkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Be användare om ålder
+            Console.Write("Hur gammal är du (heltal): ");
+            int ålder = int.Parse(Console.ReadLine());
+            
+            //Om ålder större än 18 "Du får ta körkort!"
+            if (ålder >= 18)
+            {
+                System.Console.WriteLine("Du får ta körkort!");
+            }
+
+            // Om ålder 15 eller högre "Du får ta mopedkörkort"
+            if (ålder >= 15)
+            {
+                System.Console.WriteLine("Du får ta mopedkörkort");
+            }
+            // Fråga användaren "Vad heter ABBAs senaste album?"
+            System.Console.Write("Vad heter ABBAs senaste album?");
+            string låt = Console.ReadLine();
+            
+            //är svaret korrekt?
+            if (låt == "Voyage" || låt == "voyage")
+            {
+                System.Console.WriteLine("Bra svarat!");
+            }
+            else
+            {
+                System.Console.WriteLine("Fel! Albummet heter Voyage.");
+            }
+
+            //Sista TP-fråga
+            System.Console.Write("Vem missade straffen i matchen England-Frankrike? (efternamn): ");
+
+            // Läs in och tvinga till små bokstäver
+            string spelare = Console.ReadLine().ToLower();
+            
+            if (spelare == "mbappe")
+            {
+                System.Console.WriteLine("Bra, du är en expert");
+            }
+            else
+            {
+                System.Console.WriteLine("Fel det var Mbappe");
+            }
+        }
+    }
+}
