ac186d3e018c437941ed83cb2342a6356f00f977
BlendaJensen <fredrikssonjensen.blenda@elev.ga.ntig.se>
2021-09-29 11:05:06 +0200

    Tränat på && och ||

---
+++ Kapitel 3/BoolskaOpwrationer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoolskaOpwrationer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coronasymptom");
+
+            Console.WriteLine("Har du feber? (j/n) ");
+            string feber = Console.ReadLine();
+
+            Console.WriteLine("Har du tappat smaken (j/n)");
+            string smak = Console.ReadLine();
+            
+            Console.WriteLine("Har du hosta? (j/n)");
+            string hosta = Console.ReadLine();
+      
+            if (feber == "j"|| hosta == "j" && smak == "j")
+            {
+                Console.WriteLine("Du kan ha Covid-19");
+            }
+        }
+    }
+}
