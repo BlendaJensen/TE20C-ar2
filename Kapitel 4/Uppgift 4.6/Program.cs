9b2932b1fcd7acead2f5f47eb69228a85374832b
BlendaJensen <fredrikssonjensen.blenda@elev.ga.ntig.se>
2021-10-20 11:50:26 +0200

    Övat på Char

---
+++ Kapitel 4/Uppgift 4.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string meddelande = "Hej detta är ett meddelande!";
+
+            for (int i = meddelande.Length - 1; i >= 0; i--)
+            {
+                System.Console.Write(meddelande[i]);
+            }
+        }
+    }
+}
---
+++ Kapitel 4/Uppgift 4.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriv in din summa
+            Console.WriteLine("Skriv in din summa");
+            string summaText = Console.ReadLine();
+
+            int summa = 0;
+            
+            //Loopa igenom summaText
+            for (var i = 0; i < summaText.Length; i++)
+            {
+                char siffra = summaText[i];
+                Console.WriteLine(siffra);
+
+                //Omvandla char till string till int
+                //Omvandla char med .ToString()
+                int siffraTal = int.Parse(siffra.ToString()); 
+
+                summa += siffraTal;              
+            }
+
+            Console.WriteLine($"Siffersumman av talet {summaText} är {summa}");
+        }
+    }
+}
