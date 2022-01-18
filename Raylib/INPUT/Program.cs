7b6fe2d7ab692fa7153f83eede53a6c2d040a3f3
BlendaJensen <fredrikssonjensen.blenda@elev.ga.ntig.se>
2022-01-18 11:04:39 +0100

    tränade på bilder input

---
+++ Raylib/INPUT/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Raylib_cs;
+
+namespace INPUT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+            
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            //skapa en spelare av en rektangel
+            Rectangle spelare = new Rectangle(500, 100, 50, 50);¨
+
+            //ladda bild spelare
+            Texture2D raket = Raylib.LoadTexture(@"./resurser/raket.png");
+
+            //skapa en fiende av en rektangel
+            Rectangle fiende = new Rectangle(100, 100, 50, 50);
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                /*Rita ut grafiken */
+                // Börja rita
+                Raylib.BeginDrawing();
+                
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                //rita ut spelarens bild istället för rektangel
+
+                // rita spelaren
+                Raylib.DrawRectangleRec(spelare, Color.RED);
+
+                //rita fiende
+                Raylib.DrawRectangleRec(fiende, Color.BLACK);
+                
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+
+                /*Interaktion med användaren*/
+                //lyssna på tangenter spelare
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    spelare.x += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    spelare.x -= 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    spelare.y += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    spelare.y -= 2;
+                }
+
+                //lyssna på tangenter fiende
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                {
+                    fiende.x += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                {
+                    fiende.x -= 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                {
+                    fiende.y += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                {
+                    fiende.y -= 2;
+                }
+            }
+        }
+    }
+}
---
+++ Raylib/bilder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Raylib_cs;
+
+namespace bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+            
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Ladda in bilden
+            Texture2D flygplan = Raylib.LoadTexture(@"./resurser/Flygplan.png");
+            Texture2D moon = Raylib.LoadTexture(@"./resurser/moon.png");
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Börja rita
+                Raylib.BeginDrawing();
+                
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                //rita ut bild
+                Raylib.DrawTexture(flygplan, 100, 200, Color.WHITE);
+                Raylib.DrawTexture(moon, 500, 30, Color.WHITE);
+                
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+                }
+        }
+    }
+}
